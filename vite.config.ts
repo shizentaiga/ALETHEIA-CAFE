fd15e55ecd182b9d6dcf1241ab91bb921fbaf764
shizentaiga <tshizen2506@gmail.com>
2026-04-27 22:16:09 +0900

    index.htmlが存在しない構成を設定値に追加

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import { defineConfig } from 'vite'
 import ssrPlugin from 'vite-ssr-components/plugin'
 
 export default defineConfig({
-  plugins: [cloudflare(), ssrPlugin()]
+  plugins: [
+    cloudflare(), 
+    ssrPlugin()
+  ],
+  build: {
+    rollupOptions: {
+      /**
+       * index.htmlが存在しない構成のため、
+       * 明示的にアプリケーションのエントリポイントを指定します。
+       */
+      input: './src/index.tsx'
+    }
+  }
 })
