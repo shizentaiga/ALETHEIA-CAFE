5d4ba56edb0fa7f446821f394ba416059f35944d
shizentaiga <tshizen2506@gmail.com>
2026-05-13 18:59:07 +0900

    JSコメントも削除する処理を追加

--- src/middleware/htmlMinifier.ts
+++ src/middleware/htmlMinifier.ts
@@ -14,11 +14,12 @@ export const htmlMinifier = (): MiddlewareHandler => {
       const html = await c.res.text()
       
       const minifiedHtml = html
-        .replace(/<!--[\s\S]*?-->/g, '')     // HTMLコメント削除
-        .replace(/\/\*[\s\S]*?\*\//g, '')   // CSS内のブロックコメント削除
-        .replace(/^\s+|\s+$/gm, '')         // 行頭・行末の空白削除
-        .replace(/\n/g, '')                 // 改行の削除
-        .replace(/\s{2,}/g, ' ')            // 連続する空白を1つに集約
+        .replace(/<!--[\s\S]*?-->/g, '')     // 1. HTMLコメント
+        .replace(/\/\*[\s\S]*?\*\//g, '')   // 2. CSS/JS ブロックコメント
+        .replace(/\n\s*\/\/.*$/gm, '')      // 3. JS 1行コメント (行頭・改行後の // を削除)
+        .replace(/^\s+|\s+$/gm, '')         // 4. 行頭・行末の空白削除
+        .replace(/\n/g, '')                 // 5. 改行の削除
+        .replace(/\s{2,}/g, ' ')            // 6. 連続する空白を1つに集約
 
       c.res = new Response(minifiedHtml, c.res)
     }
