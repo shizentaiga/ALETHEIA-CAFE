027aa55b3c2c22fffe89c6d3297099a82ccabefb
shizentaiga <tshizen2506@gmail.com>
2026-05-20 18:38:45 +0900

    スタバは詳細情報は取得なし

---
+++ scripts/brands/001_starbucks/001-1-2_stabucks_fetch_detail.ts
@@ -0,0 +1,9 @@
+//  npx tsx scripts/brands/001_starbucks/001-1-2_stabucks_fetch_detail.ts
+
+// scripts/data/raw/001_starbucks.json
+// 上記より、ストアIDを取得(以下サンプル)
+//       "store_id": "333"
+
+// 店舗詳細ページに移動(以下サンプル)
+// https://store.starbucks.co.jp/detail-333/
+// https://store.starbucks.co.jp/detail-4088/
